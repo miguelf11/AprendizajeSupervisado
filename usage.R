5a93a829c27e0a43979533f96c50587cc042bbdc
miguelf11 <miguelachof11@gmail.com>
2016-03-04 00:23:07 -0430

    V1

--- usage.R
+++ usage.R
@@ -1,13 +1,23 @@
-
 # Seleccionar google_api.R en su sistema de archivos
 source(file.choose())
 
 origen = c("Via Paolo Emilio", "Vancouver BC", "Seattle")
 destino =c("Piazzale Aldo Moro", "San Francisco", "Victoria BC")
 
+origen = c("Via Paolo Emilio")
+destino = c("Sapienza-Università di Roma")
+
+
 # Colocar su API Key 
-api_key = "AIzaSyBgTslqWxmNPlZx9-LvmeDrGRnVq8AEXyI"
+api_key = "AIzaSyB6ybttc49Jy60JhSzXRrko5jtK6z7ExEw"
 
-api_url = get_url(origen, destino, api_key)
+api_url = get_url(origen, destino, api_key, mode = "driving")
 
-datos = get_data(api_url)
+datos <- get_data(api_url)
+datos
+
+
+#driving
+#walking 
+#bicycling
+#transit 
